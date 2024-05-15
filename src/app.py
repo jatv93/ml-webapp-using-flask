e67fa03cff0495c4874f617045ff38c6774615ab
jatv93 <toledojennifer20@gmail.com>
2024-05-15 22:55:01 +0000

    finished

--- src/app.py
+++ src/app.py
@@ -1,4 +1,32 @@
-from utils import db_connect
-engine = db_connect()
+import numpy as np
+import flask
+from flask import Flask, render_template, request
+from pickle import load
 
-# your code here
+app = Flask(__name__)
+model = load(open("../models/tree_classifier_crit-entro_maxdepth-5_minleaf-4_minsplit2_42.sav", "rb"))
+class_dict = {
+    "0": "Negative Possibility",
+    "1": "Positive Possibility"
+}
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == "POST":
+        
+        # Obtain values from form
+        val1 = float(request.form["val1"])
+        val2 = float(request.form["val2"])
+        val3 = float(request.form["val3"])
+        val4 = float(request.form["val4"])
+        val5 = float(request.form["val5"])
+        val6 = float(request.form["val6"])
+        val7 = float(request.form["val7"])
+        
+        data = [[val1, val2, val3, val4, val5, val6, val7]]
+        prediction = str(model.predict(data)[0])
+        pred_class = class_dict[prediction]
+    else:
+        pred_class = None
+    
+    return render_template("index.html", prediction = pred_class)
